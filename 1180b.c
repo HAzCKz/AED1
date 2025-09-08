1a9a427ae05295cc30f6c9c38d50f18f9775e5b6
HAzCKz <pedro.augusto.ds2709@gmail.com>
2025-09-08 18:29:16 -0300

    FIX erro de formatacao

--- 1180b.c
+++ 1180b.c
@@ -1,37 +1,37 @@
-#include <stdio.h>
+    #include <stdio.h>
 
-int main ()
-{
-    int N, menor, posicao;
-
-    int *tamanho = &N;
-
-    scanf("%i", tamanho);
-    
-    int X[*tamanho];
-
-    int *end_x = X;
-
-    for (int i = 0; i < *tamanho; i++)
+    int main ()
     {
-        scanf("%i", end_x);
-        end_x++;
-    }
+        int N, menor, posicao;
 
-    end_x = X;
+        int *tamanho = &N;
 
-    menor = *end_x;
+        scanf("%i", tamanho);
+        
+        int X[*tamanho];
 
-    for (int i = 0; i < *tamanho; i++)
-    {
-        if (*end_x <= menor)
+        int *end_x = X;
+
+        for (int i = 0; i < *tamanho; i++)
         {
-            menor = *end_x;
-            posicao = i;
+            scanf("%i", end_x);
+            end_x++;
         }
-        end_x++;
-    }
 
-    printf("Menor: %i\n", menor);
-    printf("Posicao %i\n", posicao);
-}
+        end_x = X;
+
+        menor = *end_x;
+
+        for (int i = 0; i < *tamanho; i++)
+        {
+            if (*end_x <= menor)
+            {
+                menor = *end_x;
+                posicao = i;
+            }
+            end_x++;
+        }
+
+        printf("Menor valor: %i\n", menor);
+        printf("Posicao: %i\n", posicao);
+    }
