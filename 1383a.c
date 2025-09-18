f31729355688b90c3801d05410396a5e879b8b83
HAzCKz <pedro.augusto.ds2709@gmail.com>
2025-09-18 19:53:15 -0300

    ADD leitura e escrita do vetor 3D

--- 1383a.c
+++ 1383a.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 
+#define LINHAS 3
+#define COLUNAS 3
+
 int main()
 {
+    int instancia = 0, k = 0, l = 0;    
+
+    scanf("%i", &instancia);
+
+    int matriz[instancia][LINHAS][COLUNAS];
+
+    while (k < instancia)
+    {
+        for (int i = 0; i < LINHAS; i++)
+        {
+            for (int j = 0; j < COLUNAS; j++)
+            {
+                scanf("%i", &matriz[k][i][j]);
+            }
+        }
+        k++;
+    }
+
+    while (l < instancia)
+    {
+        for (int i = 0; i < LINHAS; i++)
+        {
+            for (int j = 0; j < COLUNAS; j++)
+            {
+                printf("%i", matriz[l][i][j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+        l++;
+    }
+    
+
     
 }
