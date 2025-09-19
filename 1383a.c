b52c7c9fb3c2e3fa546dafce2c8642b7d4183fe1
HAzCKz <pedro.augusto.ds2709@gmail.com>
2025-09-18 21:41:29 -0300

    ADD verificao das linhas

--- 1383a.c
+++ 1383a.c
@@ -5,13 +5,15 @@
 
 int main()
 {
-    int instancia = 0, k = 0, l = 0;    
+    int instancia = 0, k = 0, l = 0, m = 0;  
+    
+    int valor_coluna = 0, valor_linha = 0, verifica = 0, erro = 0;
 
     scanf("%i", &instancia);
 
     int matriz[instancia][LINHAS][COLUNAS];
 
-    while (k < instancia)
+    while (k < instancia) // Leitura dos elementos das matrizes
     {
         for (int i = 0; i < LINHAS; i++)
         {
@@ -23,7 +25,29 @@ int main()
         k++;
     }
 
-    while (l < instancia)
+    int ii = 0, jj = 0;
+
+    while (m < instancia)
+    {
+        for (ii = 0; ii < LINHAS; ii++)
+        {
+
+            valor_linha = matriz[m][ii][0];
+            
+            for (jj = 1; jj < COLUNAS; jj++)
+            {                
+                if(valor_linha == matriz[m][ii][jj])
+                {
+                    erro = 1;
+                }
+            }
+            
+        }
+        m++;
+    }
+
+
+    while (l < instancia) // Exibicao das matrizes
     {
         for (int i = 0; i < LINHAS; i++)
         {
@@ -35,6 +59,15 @@ int main()
         }
         printf("\n");
         l++;
+
+        if (erro == 0)
+        {
+            printf("O sudoku esta correto!\n");
+        }
+        else
+        {
+            printf("O sudoku esta errado!\n");
+        }
     }
     
 
