36d7e94132926fbbed8780b8108a7fb42487947f
HAzCKz <pedro.augusto.ds2709@gmail.com>
2025-10-03 10:37:56 -0300

    CREATE 1069.c

---
+++ 1069.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int N; 
+    scanf("%d\n", &N);
+    
+
+    while (N > 0)
+    {
+        int push = 0, total = 0;
+        char diamantes[1000];
+
+        fgets(diamantes, sizeof(diamantes), stdin);
+
+        for (int i = 0, n = strlen(diamantes); i < n; i++)
+        {
+            if (diamantes[i] == '<')
+            {
+                push++; 
+            }
+            else if (diamantes[i] == '>' && push > 0)
+            {
+                push--;  
+                total++; 
+            }
+        }
+
+        printf("%d\n", total);
+        N--;
+    }
+
+    return 0;
+}
