f464dea93d154acd5b2dae411c90f20bb94bb958
HAzCKz <pedro.augusto.ds2709@gmail.com>
2025-11-29 17:33:14 -0300

    UPDATE implementando algoritmo de ordenação

--- 1548.c
+++ 1548.c
@@ -5,28 +5,46 @@ int main()
 {
     int N = 0;
 
-    scanf("%d", &N);
+    //scanf("%d", &N);
 
     //while (N > 0)
-    /{
+    //{
         int num_alunos = 0;
 
         scanf("%d", &num_alunos);
 
-        int *notas = malloc(sizeof(int)*num_alunos);
-        int *notas_org = malloc(sizeof(int)*num_alunos);
+        int notas[num_alunos];
+        int notas_org[num_alunos];
 
-
-        for (int i = 0; num_alunos > i; i++)
+        for (int i = 0; i < num_alunos; i++)
         {
-            scanf("%d", (notas+i));
+            scanf("%d", &notas[i]);
+        }
+    
+        for (int i = 0; i < num_alunos; i++)
+        {
+            notas_org[i] = notas[i];
+        }
+
+        for (int i = 0; i < num_alunos-1; i++)
+        {
+            for (int j = 0; j < num_alunos-i-1; j++) 
+            {
+                if (notas_org[j] < notas_org[j+1]) 
+                {
+                    int temp = notas_org[j];
+                    notas_org[j] = notas_org[j+1];
+                    notas_org[j+1] = temp;
+                }
+            }
         }
 
         for (int i = 0; i < num_alunos; i++)
         {
-            
+            printf("NOtas org: %d\n", notas_org[i]);
         }
-        
+
+        //printf("Maior: %d\n", maior);
         //N--;
     //}
 }
