660fb65d72900e41591736747e7c5a951be643dd
HAzCKz <pedro.augusto.ds2709@gmail.com>
2025-12-01 08:49:35 -0300

    CREATE 1566.c

---
+++ 1566.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int casos = 0;
+
+    scanf("%d", &casos);
+
+    // inserir loop while(casos > 0)
+
+    long pessoas = 0;
+
+    scanf("%d", &pessoas);
+
+    
+}
