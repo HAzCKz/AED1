b3987c4b516447e7a054de00ca18a840e8c11a93
HAzCKz <pedro.augusto.ds2709@gmail.com>
2026-01-09 00:48:39 -0300

    FINAL version

--- 1907.c
+++ 1907.c
@@ -1,69 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Estrutura para armazenar as coordenadas de cada pixel
-typedef struct {
+typedef struct Ponto{
     int x;
     int y;
 } Ponto;
 
-// Matriz global para evitar sobrecarga na pilha de execução
 char mapa[1030][1030];
-Ponto pilha[1100000]; // Pilha para a DFS (tamanho N * M)
+
+Ponto pilha[1100000];
+
 int topo = -1;
 
-void empilhar(int x, int y) {
-    topo++;
-    pilha[topo].x = x;
-    pilha[topo].y = y;
-}
+void Empilha(int x, int y);
+Ponto Desempilha();
 
-Ponto desempilhar() {
-    return pilha[topo--];
-}
 
-int main() {
-    int N, M;
+int main()
+{
+    int N = 0, M = 0;
     int cliques = 0;
 
-    // Lendo as dimensões da imagem
-    if (scanf("%d %d", &N, &M) != 2) {
+    if (scanf("%d %d", &N, &M) != 2)
+    {
         return 0;
     }
 
-    // Lendo o cenário
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++)
+    {
         scanf("%s", mapa[i]);
     }
 
-    // Percorrendo cada pixel da matriz
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
             
-            // Se encontrarmos um pixel branco '.', iniciamos uma nova coloração (clique)
-            if (mapa[i][j] == '.') {
+            if (mapa[i][j] == '.')
+            {
                 cliques++;
                 
-                // Iniciamos a DFS manual usando a nossa pilha
-                empilhar(i, j);
-                mapa[i][j] = 'o'; // Marcamos como colorido (preto) para não repetir
+                Empilha(i, j);
 
-                while (topo != -1) {
-                    Ponto atual = desempilhar();
+                mapa[i][j] = 'o'; 
 
-                    // Direções: Cima, Baixo, Esquerda, Direita
+                while (topo != -1)
+                {
+                    Ponto atual = Desempilha();
+
                     int dx[] = {-1, 1, 0, 0};
                     int dy[] = {0, 0, -1, 1};
 
-                    for (int k = 0; k < 4; k++) {
+                    for (int k = 0; k < 4; k++)
+                    {
                         int nx = atual.x + dx[k];
                         int ny = atual.y + dy[k];
 
-                        // Verificamos se o vizinho está dentro dos limites e se é branco
-                        if (nx >= 0 && nx < N && ny >= 0 && ny < M) {
-                            if (mapa[nx][ny] == '.') {
-                                mapa[nx][ny] = 'o'; // Marca antes de empilhar para evitar duplicatas na pilha
-                                empilhar(nx, ny);
+                        if (nx >= 0 && nx < N && ny >= 0 && ny < M)
+                        {
+                            if (mapa[nx][ny] == '.')
+                            {
+                                mapa[nx][ny] = 'o';
+                                Empilha(nx, ny);
                             }
                         }
                     }
@@ -73,6 +71,16 @@ int main() {
     }
 
     printf("%d\n", cliques);
+}
 
-    return 0;
+void Empilha(int x, int y)
+{
+    topo++;
+    pilha[topo].x = x;
+    pilha[topo].y = y;
+}
+
+Ponto Desempilha() 
+{
+    return pilha[topo--];
 }
--- 2729.c
+++ 2729.c
@@ -7,42 +7,58 @@ int main()
     scanf("%d", &N);
     getchar(); 
 
-    char line[10000];
-    while (N--) {
-        if (!fgets(line, sizeof(line), stdin)) break;
+    char itens[10000];
+    while (N--)
+    {
+        if (!fgets(itens, sizeof(itens), stdin)) break;
 
-        int len = strlen(line);
-        if (line[len-1] == '\n') line[len-1] = '\0';
+        int tam = strtam(itens);
 
-        char items[1000][50];
-        int count = 0;
+        if (itens[tam-1] == '\n') itens[tam-1] = '\0';
+
+        char produto[1000][50];
+        int contador = 0;
 
         int i = 0;
-        while (line[i]) {
-            int j = 0;
-                 while (line[i] && line[i] != ' ') {
-                items[count][j++] = line[i++];
-            }
-            items[count][j] = '\0';
-            count++;
-            while (line[i] == ' ') i++; 
 
-             for (int x = 0; x < count-1; x++) {
-            for (int y = x+1; y < count; y++) {
-                if (strcmp(items[x], items[y]) > 0) {
+        while (itens[i])
+        {
+            int j = 0;
+
+            while (itens[i] && itens[i] != ' ')
+            {
+                produto[contador][j++] = itens[i++];
+            }
+
+            produto[contador][j] = '\0';
+            contador++;
+
+            while (itens[i] == ' ') 
+            {
+                i++; 
+            }
+
+            for (int x = 0; x < contador-1; x++)
+            {
+            for (int y = x+1; y < contador; y++)
+            {
+                if (strcmp(produto[x], produto[y]) > 0)
+                {
                     char tmp[50];
-                    strcpy(tmp, items[x]);
-                    strcpy(items[x], items[y]);
-                    strcpy(items[y], tmp);
+                    strcpy(tmp, produto[x]);
+                    strcpy(produto[x], produto[y]);
+                    strcpy(produto[y], tmp);
                 }
             }
         }
 
         
-        for (int k = 0; k < count; k++) {
-            if (k == 0 || strcmp(items[k], items[k-1]) != 0) {
+        for (int k = 0; k < contador; k++)
+        {
+            if (k == 0 || strcmp(produto[k], produto[k-1]) != 0)
+            {
                 if (k != 0) printf(" ");
-                printf("%s", items[k]);
+                printf("%s", produto[k]);
             }
         }
         printf("\n");
